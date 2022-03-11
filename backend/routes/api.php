188d2efbee1ef53690a518ac2dcdfb8f4332f331
Manpreet Bhatti <manpreetbhatti@Manpreets-MacBook-Air.local>
2022-03-10 19:57:06 -0500

    Route added

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('api/{id}', 'ApiController@get');
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 
